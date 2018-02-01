ca9d52c963fb6f03a33a72d692062719e7ac9716
Marcus Ramsden <marcus.ramsden@gmail.com>
2018-02-01 23:43:59 +0000

    Trying out some teamcity formatting

--- Makefile
+++ Makefile
@@ -6,8 +6,16 @@ xcode_test=xcodebuild test \
 	-derivedDataPath ./build/DerivedData \
 	-workspace ReactiveLogin.xcworkspace \
 	-scheme ReactiveLogin \
-	-destination '$(test_destination)'
-xcpretty=bundle exec xcpretty
+	-destination '$(test_destination)' \
+	-parallelizeTargets
+xcode_archive=xcodebuild archive \
+	-derivedDataPath ./build/DerivedData \
+	-archivePath ./build/Archives/ReactiveLogin \
+	-workspace ReactiveLogin.xcworkspace \
+	-scheme ReactiveLogin \
+	-configuration Release \
+	-parallelizeTargets
+xcpretty=bundle exec xcpretty -f ./teamcity_formatter.rb
 
 test: clean-reports
 	$(xcode_test) | $(xcpretty) -r junit -r html
@@ -15,13 +23,19 @@ test: clean-reports
 test-ci:
 	$(xcode_test) | $(xcpretty)
 
-clean: clean-reports clean-derived-data
+archive:
+	$(xcode_archive) | $(xcpretty)
+
+clean: clean-reports clean-derived-data clean-archives
 
 clean-reports:
 	rm -rf build/reports
 
 clean-derived-data:
 	rm -rf build/DerivedData
+
+clean-archives:
+	rm -rf build/Archives
 
 setup:
 	bundle install
---
+++ teamcity_formatter.rb
@@ -0,0 +1,95 @@
+
+class TeamCityFormatter < XCPretty::Simple
+  EMPTY = "".freeze
+
+  def format_build_target(target, project, configuration)
+    if @previous_target != nil
+      build_target_finished(@previous_target)
+      @previous_target = nil
+    end
+
+    "##teamcity[blockOpened name='#{target}']"
+  end
+
+  def build_target_finished(target)
+    "##teamcity[blockClosed name='#{target}']"
+  end
+
+  def format_linking(target, build_variants, arch)
+    "##teamcity[progressMessage 'Linking #{target}'"    
+  end
+
+  # Used when opening a test target
+  # For example: Test Suite 'Unit Tests.xctest' started at ...
+  def format_test_run_started(name)
+    "##teamcity[testSuiteStarted name='#{name}']"
+  end
+
+  # Used to signal a test target is complete
+  def format_test_run_finished(name, time)
+
+    if @previousSuite != nil
+      # Close the last test suite before the target is complete
+      testSuiteFinished(@previousSuite)
+      @previousSuite = nil
+    end
+
+    "##teamcity[testSuiteFinished name='#{name}']"
+  end
+
+  def format_test_suite_started(name)
+    #Skip the All tests, which comes first
+    return "" if name == 'All tests'
+
+    if @previousSuite != nil
+       testSuiteFinished(@previousSuite)
+    end
+
+    @previousSuite = name
+    "##teamcity[testSuiteStarted name='#{name}']"
+  end
+
+  def testSuiteFinished(name)
+    puts "##teamcity[testSuiteFinished name='#{@previousSuite}']"
+  end
+
+  def format_passing_test(suite, test, time)
+    "##teamcity[testStarted name='#{test}']\n" +
+        "##teamcity[testFinished name='#{test}' duration='#{time}']"
+  end
+
+  def format_failing_test(suite, test, time, file_path)
+    "##teamcity[testStarted name='#{test}']\n" +
+        "##teamcity[testFailed name='#{test}' message='#{time}']\n" +
+        "##teamcity[testFinished name='#{test}']"
+  end
+
+  def format_warning(message)
+    "##teamcity[message text='#{message}' status='warning']"    
+  end
+
+  def format_error(message)
+    "##teamcity[testStdErr name='className.testName' out='#{message}']"
+  end
+
+  def format_check_dependencies; EMPTY; end
+
+  def format_build_target(target, project, configuration)
+    "##teamcity[progressMessage 'Building #{target}']"
+  end
+
+  def format_compile(file_name, file_path)
+    "##teamcity[progressMessage 'Compiling #{file_name}']"
+  end
+
+  def format_touch(file_path, file_name)
+    "##teamcity[progressMessage 'Touching #{file_name}']"
+  end
+
+  def format_phase_success(phase_name)
+    "##teamcity[progressMessage '#{phase_name} Success']"
+  end
+
+end
+
+TeamCityFormatter
